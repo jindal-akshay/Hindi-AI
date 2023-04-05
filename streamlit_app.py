8da9a7d6a6ac19475e63375d797adc15d7688b11
Akshay Jindal <intendefer@gmail.com>
2023-04-05 19:33:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,35 +5,20 @@ from io import BytesIO
 # Set OpenAI API key
 openai.api_key = st.secrets["api_secrets"]
 
-# Define Streamlit app title and description
-st.set_page_config(page_title="Audio Translator", page_icon=":sound:", layout="wide")
-st.title("Audio Translator")
-st.write("This app allows you to upload an MP3 file and translate it into English text using OpenAI API.")
+# Define app header
+st.set_page_config(page_title="Hindi Audio to English Text", page_icon=":microphone:", layout="wide")
 
-# Display a file uploader in Streamlit
-audio_file = st.file_uploader("Upload an MP3 file", type=["mp3"])
+st.title("Hindi Audio to English Text")
+st.markdown("""
+    This app uses OpenAI to translate Hindi audio to English text. 
+    Simply upload an mp3 file and the app will generate a transcript in English.
+""")
 
-# Check if a file was uploaded
-if audio_file:
-    # Read the uploaded file
-    audio_bytes = audio_file.read()
-    
-    # Convert the bytes to a file-like object
-    audio_fileobj = BytesIO(audio_bytes)
-    
-    # Set the name attribute of the file object
-    audio_fileobj.name = audio_file.name
-    
-    # Use OpenAI API to translate audio
-    response = openai.Audio.create(model="whisper-1", media=audio_fileobj)
-    
-    # Extract the transcript text from the response
-    transcript = response['transcript']
-    
-    # Display the transcript in Streamlit
-    st.write("Transcript:")
-    st.write(transcript)
-    
-    # Create a downloadable link to save the transcript as .txt file
-    transcript_file = transcript.encode('utf-8')
-    st.download_button('Download Transcript', data=transcript_file, file_name='transcript.txt', mime='text/plain')
+# Display file uploader
+audio_file = st.file_uploader("Upload an mp3 file", type=["mp3"])
+
+# Translate audio to text and display result
+if audio_file is not None:
+    transcript = openai.Audio.translate("whisper-1", audio_file.read(), target_language="en")
+    st.header("Transcript:")
+    st.text(transcript)
