021e62e8fba3621e6bdd1471e83ed598d975d7a2
Akshay Jindal <intendefer@gmail.com>
2023-04-05 20:38:11 -0400

    tes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,35 @@
-import openai
 import streamlit as st
-from io import BytesIO
 from pydub import AudioSegment
+from io import BytesIO
+import base64
+import mediainfo
 
-# Set OpenAI API key s
-openai.api_key = st.secrets["api_secrets"]
+st.set_page_config(page_title="Audio Player")
 
-# Define app header
-st.set_page_config(page_title="Hindi Audio to English Text", page_icon=":microphone:", layout="wide")
+def main():
+    st.title("Audio Player")
 
-st.title("Hindi Audio to English Text")
-st.markdown("""
-    This app uses OpenAI to translate Hindi audio to English text. 
-    Simply upload an mp3 or wav file and the app will generate a transcript in English.
-""")
+    audio_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "ogg"])
 
-# Display file uploader
-audio_file = st.file_uploader("Upload an mp3 or wav file", type=["mp3", "wav"])
+    if audio_file is not None:
+        try:
+            audio_bytes = audio_file.read()
+            audio = AudioSegment.from_file(BytesIO(audio_bytes), format=audio_file.type)
+            st.audio(audio_bytes, format=audio_file.type)
+            st.success("Audio file uploaded successfully!")
+        except Exception as e:
+            st.error("Error occurred while processing the audio file.")
+            st.error(str(e))
+            
+        try:
+            # Get metadata of the uploaded file
+            metadata = mediainfo.analyze(audio_file)
+            st.write("Metadata:")
+            for key, value in metadata.to_data().items():
+                st.write(f"{key}: {value}")
+        except Exception as e:
+            st.error("Error occurred while getting metadata of the audio file.")
+            st.error(str(e))
 
-# Translate audio to text and display result
-if audio_file is not None:
-    # Convert file contents to bytes
-    audio_bytes = BytesIO(audio_file.read()).getvalue()
-
-    # Load audio file using PyDub
-    audio = AudioSegment.from_file(BytesIO(audio_bytes), format=audio_file.type)
-
-    # Extract audio data as raw PCM
-    raw_audio_data = audio.raw_data
-
-    # Send audio to OpenAI to generate transcript
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=f"Please translate the following Hindi audio to English:\n\n{raw_audio_data.decode('utf-8')}\n",
-        temperature=0.8,
-        max_tokens=2048,
-        n = 1,
-        stop=None,
-        timeout=60,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    transcript = response.choices[0].text.strip()
-
-    st.header("Transcript:")
-    st.text(transcript)
+if __name__ == "__main__":
+    main()
