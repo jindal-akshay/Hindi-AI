a22199287e56b49522b82038aab148b5b015e911
Akshay Jindal <intendefer@gmail.com>
2023-04-05 20:49:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,15 +14,15 @@ audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
 model = whisper.load_model("base")
 st.text("whisper model loaded")
 
-
 if st.sidebar.button("transcribe audio"):
-	if audio_file is not None:
-		st.sidebar.success("Transcribing audio")
-		transcription = model.transcribe(audio_file.name)
-		st.sidebar.success("Transcription complete")
-		st.markdown(transcription["text"])
-	else:
-		st.sidebar.error("Please upload file")
+    if audio_file is not None:
+        st.sidebar.success("Transcribing audio")
+        transcription = model.transcribe(audio_file.name)
+        st.sidebar.success("Transcription complete")
+        st.markdown(transcription["text"])
+    else:
+        st.sidebar.error("Please upload file")
 
-st.sidebar.header("play audio file")
-st.sidebar.audio(audio_file)
+if audio_file is not None:
+    st.sidebar.header("play audio file")
+    st.sidebar.audio(audio_file)
