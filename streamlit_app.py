cbdb03b9da82d426195b07795497d7d8042b03c6
Akshay Jindal <intendefer@gmail.com>
2023-04-07 20:42:00 -0400

    test
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,39 @@
-import openai
 import streamlit as st
-from io import BytesIO
 from pydub import AudioSegment
+import io
+import openai
+import os
 
-# Set OpenAI API key
 openai.api_key = st.secrets["api_secrets"]
-
 # Define app header
 st.set_page_config(page_title="Hindi Audio to English Text", page_icon=":microphone:", layout="wide")
 
 st.title("Hindi Audio to English Text")
 st.markdown("""
-    pls enter your hindi audio below.
+    please enter your hindi audio below and also make sure that the audio is in either mp3 or wav format.
 """)
 
-# Display file uploader
-audio_file = st.file_uploader("Upload an mp3 or wav file", type=["mp3", "wav"])
+# file uploader
+uploaded_file = st.file_uploader("Upload an mp3 or wav file", type=["mp3", "wav"])
 
-# Translate audio to text and display result
-if audio_file is not None:
-    # Convert file contents to bytes
-    audio_bytes = BytesIO(audio_file.read()).getvalue()
+if uploaded_file:
+    # Read the content of the uploaded file
+    file_content = uploaded_file.read()
 
-    # Load audio file using PyDub
-    audio = AudioSegment.from_file(BytesIO(audio_bytes), format=audio_file.type)
+    with open("test.mp3", "wb") as f:
+        audio_file = f.write(file_content)
+    
+    st.write("Filename:", uploaded_file.name)
+    audio_format = uploaded_file.type.split('/')[1]  # Get the file format, either 'mp3' or 'wav'
+    audio = open("test.mp3", 'rb')
+    # Play the uploaded audio file
+    st.audio(file_content, format=f'audio/{audio_format}')
 
-    # Extract audio data as raw PCM
-    raw_audio_data = audio.raw_data
-
-    # Send audio to OpenAI to generate transcript
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=f"Please translate the following Hindi audio to English:\n\n{raw_audio_data.decode('utf-8')}\n",
-        temperature=0.8,
-        max_tokens=2048,
-        n = 1,
-        stop=None,
-        timeout=60,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    transcript = response.choices[0].text.strip()
-
+    # Get the transcript
+    transcript = openai.Audio.translate("whisper-1", audio, "This transcript is in Hindi.")
     st.header("Transcript:")
-    st.text(transcript)
+    st.markdown(transcript["text"])
+    print(transcript)
+    # close the file
+    audio.close()
+    os.remove("test.mp3")
