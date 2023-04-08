68d54d0f109f2556c1fc7c6944056034d7c0c3cd
Akshay Jindal <intendefer@gmail.com>
2023-04-07 21:32:08 -0400

    test
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,45 +3,36 @@ from pydub import AudioSegment
 import openai
 import os
 
-# Set OpenAI API key
 openai.api_key = st.secrets["api_secrets"]
+# Define app header
+st.set_page_config(page_title="EZ hindi 2 english", page_icon=":microphone:", layout="wide")
 
-# Set app header and page configuration
-st.set_page_config(
-    page_title="EZ Hindi 2 English",
-    page_icon=":microphone:",
-    layout="wide",
-    page_bg_color='#f7f7f7',
-    )
-
-# Set app header and description
-st.title("EZ Hindi 2 English")
+st.title("EZ hindi 2 english")
 st.markdown("""
-    Don't mess it up. Upload an audio file in Hindi and get the English transcript. It's simple.
+    dont fuck it up. Enter your audio and get english translation. simple.
 """)
 
-# Create file uploader
+# file uploader
 uploaded_file = st.file_uploader("Upload an mp3 or wav file", type=["mp3", "wav"])
 
 if uploaded_file:
     # Read the content of the uploaded file
     file_content = uploaded_file.read()
 
-    # Save the uploaded file to disk
     with open("test.mp3", "wb") as f:
         audio_file = f.write(file_content)
-
-    # Display the uploaded file name and play the audio file
+    
     st.write("Filename:", uploaded_file.name)
-    audio_format = uploaded_file.type.split('/')[1]
+    audio_format = uploaded_file.type.split('/')[1]  # Get the file format, either 'mp3' or 'wav'
+    audio = open("test.mp3", 'rb')
+    # Play the uploaded audio file
     st.audio(file_content, format=f'audio/{audio_format}')
 
     # Get the transcript
-    audio = open("test.mp3", 'rb')
     transcript = openai.Audio.translate("whisper-1", audio, "This transcript is in Hindi.")
     st.header("Transcript:")
     st.markdown(transcript["text"])
-
-    # Close the file and delete it from disk
+    print(transcript)
+    # close the file
     audio.close()
     os.remove("test.mp3")
