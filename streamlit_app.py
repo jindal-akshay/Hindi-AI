e4e9273987c0306e9d7c0fe07d38b4316beeed00
Akshay Jindal <intendefer@gmail.com>
2023-04-07 21:29:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,53 +2,46 @@ import streamlit as st
 from pydub import AudioSegment
 import openai
 import os
-from streamlit_themes import st_theme
 
+# Set OpenAI API key
 openai.api_key = st.secrets["api_secrets"]
-# Define app header
-st.set_page_config(page_title="EZ hindi 2 english", page_icon=":microphone:", layout="wide", page_bg_color='#f7f7f7')
 
-# Set a custom CSS style for the app
-st.markdown("""
-        <style>
-        body {
-            background-color: #f7f7f7;
-        }
-        .stApp {
-            background-color: #fff;
-            padding: 2rem;
-            border-radius: 1rem;
-            box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
-        }
-        </style>
-        """, unsafe_allow_html=True)
+# Set app header and page configuration
+st.set_page_config(
+    page_title="EZ Hindi 2 English",
+    page_icon=":microphone:",
+    layout="wide",
+    page_bg_color='#f7f7f7',
+    )
 
-st.title("EZ hindi 2 english")
+# Set app header and description
+st.title("EZ Hindi 2 English")
 st.markdown("""
-    dont fuck it up. Enter your audio and get english translation. simple.
+    Don't mess it up. Upload an audio file in Hindi and get the English transcript. It's simple.
 """)
 
-# file uploader
+# Create file uploader
 uploaded_file = st.file_uploader("Upload an mp3 or wav file", type=["mp3", "wav"])
 
 if uploaded_file:
     # Read the content of the uploaded file
     file_content = uploaded_file.read()
 
+    # Save the uploaded file to disk
     with open("test.mp3", "wb") as f:
         audio_file = f.write(file_content)
 
+    # Display the uploaded file name and play the audio file
     st.write("Filename:", uploaded_file.name)
-    audio_format = uploaded_file.type.split('/')[1]  # Get the file format, either 'mp3' or 'wav'
-    audio = open("test.mp3", 'rb')
-    # Play the uploaded audio file
+    audio_format = uploaded_file.type.split('/')[1]
     st.audio(file_content, format=f'audio/{audio_format}')
 
     # Get the transcript
+    audio = open("test.mp3", 'rb')
     transcript = openai.Audio.translate("whisper-1", audio, "This transcript is in Hindi.")
     st.header("Transcript:")
     st.markdown(transcript["text"])
-    print(transcript)
-    # close the file
+
+    # Close the file and delete it from disk
     audio.close()
     os.remove("test.mp3")
