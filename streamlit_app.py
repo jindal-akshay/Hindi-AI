6e2b6f2e2de6bd04be15fe7590f0d3db6f760c74
Akshay Jindal <intendefer@gmail.com>
2023-04-19 21:28:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,62 +1,32 @@
 import streamlit as st
-from pydub import AudioSegment
-import openai
-import os
-import requests
-from io import BytesIO
-import urllib.request
 import pafy
 
-openai.api_key = st.secrets["api_secrets"]
-# Define app header
-st.set_page_config(page_title="EZ hindi 2 english", page_icon=":microphone:", layout="wide")
+# Title of the web app
+st.title("YouTube Video Downloader")
 
-st.title("EZ hindi 2 english")
-st.markdown("""
-    Enter your audio and get English translation. Simple!
-""")
+# Get YouTube link from user input
+yt_link = st.text_input("Enter the YouTube video link:")
 
-# file uploader
-option = st.selectbox("Select a source", ["Upload an mp3 or wav file", "Enter a YouTube link"])
-if option == "Upload an mp3 or wav file":
-    uploaded_file = st.file_uploader("Upload an mp3 or wav file", type=["mp3", "wav"])
-
-    if uploaded_file:
-        # Read the content of the uploaded file
-        file_content = uploaded_file.read()
-
-        with open("test.mp3", "wb") as f:
-            audio_file = f.write(file_content)
-
-        st.write("Filename:", uploaded_file.name)
-        audio_format = uploaded_file.type.split('/')[1]  # Get the file format, either 'mp3' or 'wav'
-        audio = open("test.mp3", 'rb')
-        # Play the uploaded audio file
-        st.audio(file_content, format=f'audio/{audio_format}')
-
-        # Get the transcript
-        transcript = openai.Audio.translate("whisper-1", audio, "This transcript is in Hindi.")
-        st.header("Transcript:")
-        st.markdown(transcript["text"])
-        print(transcript)
-        # close the file
-        audio.close()
-        os.remove("test.mp3")
-
-elif option == "Enter a YouTube link":
-    yt_link = st.text_input("Enter a YouTube link")
-    if yt_link:
+# If user has provided a link
+if yt_link:
+    try:
+        # Create a Pafy object
         video = pafy.new(yt_link)
-        audio_stream = video.getbestaudio()
-        audio_url = audio_stream.url
-        audio_content = requests.get(audio_url).content
 
-        st.write("Video Title:", video.title)
-        st.audio(audio_content, format="audio/mp3")
+        # Display video details
+        st.write(f"Title: {video.title}")
+        st.write(f"Author: {video.author}")
+        st.write(f"Duration: {video.duration}")
 
-        # Get the transcript
-        audio = BytesIO(audio_content)
-        transcript = openai.Audio.translate("whisper-1", audio, "This transcript is in Hindi.")
-        st.header("Transcript:")
-        st.markdown(transcript["text"])
-        print(transcript)
+        # Get best available resolution
+        streams = video.streams
+        resolutions = [stream.resolution for stream in streams if stream.resolution]
+        selected_resolution = st.selectbox("Select resolution:", resolutions)
+        stream = video.getbest(preftype="mp4", ftypestrict=True, resolution=selected_resolution)
+
+        # Download video
+        st.write("Downloading...")
+        filename = stream.download()
+        st.write("Download complete!")
+    except (OSError, IOError):
+        st.error("The video is either private or has been removed from YouTube. Please try another video.")
