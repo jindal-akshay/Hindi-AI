4baff5b026aacad43c7f6b4a7ece7529d2450f33
Akshay Jindal <intendefer@gmail.com>
2023-04-19 21:42:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,62 @@
 import streamlit as st
-import pafy
-import yt_dlp
-
-ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
-    from __future__ import unicode_literals
-    import youtube_dl
-    ydl_opts = {
-         'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192'
-        }],
-        'postprocessor_args': [
-            '-ar', '16000'
-        ],
-        'prefer_ffmpeg': True,
-        'keepvideo': True
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(['link'])
-
-use this:
-from pytube import YouTube
+from pydub import AudioSegment
+import openai
 import os
+import requests
+from io import BytesIO
+import urllib.request
+import pafy
 
-yt = YouTube('link')
+openai.api_key = st.secrets["api_secrets"]
+# Define app header
+st.set_page_config(page_title="EZ hindi 2 english", page_icon=":microphone:", layout="wide")
 
-video = yt.streams.filter(only_audio=True).first()
+st.title("EZ hindi 2 english")
+st.markdown("""
+    Enter your audio and get English translation. Simple!
+""")
 
-out_file = video.download(output_path=".")
+# file uploader
+option = st.selectbox("Select a source", ["Upload an mp3 or wav file", "Enter a YouTube link"])
+if option == "Upload an mp3 or wav file":
+    uploaded_file = st.file_uploader("Upload an mp3 or wav file", type=["mp3", "wav"])
 
-base, ext = os.path.splitext(out_file)
-new_file = base + '.mp3'
-os.rename(out_file, new_file)
+    if uploaded_file:
+        # Read the content of the uploaded file
+        file_content = uploaded_file.read()
+
+        with open("test.mp3", "wb") as f:
+            audio_file = f.write(file_content)
+
+        st.write("Filename:", uploaded_file.name)
+        audio_format = uploaded_file.type.split('/')[1]  # Get the file format, either 'mp3' or 'wav'
+        audio = open("test.mp3", 'rb')
+        # Play the uploaded audio file
+        st.audio(file_content, format=f'audio/{audio_format}')
+
+        # Get the transcript
+        transcript = openai.Audio.translate("whisper-1", audio, "This transcript is in Hindi.")
+        st.header("Transcript:")
+        st.markdown(transcript["text"])
+        print(transcript)
+        # close the file
+        audio.close()
+        os.remove("test.mp3")
+
+elif option == "Enter a YouTube link":
+    yt_link = st.text_input("Enter a YouTube link")
+    if yt_link:
+        video = pafy.new(yt_link)
+        audio_stream = video.getbestaudio()
+        audio_url = audio_stream.url
+        audio_content = requests.get(audio_url).content
+
+        st.write("Video Title:", video.title)
+        st.audio(audio_content, format="audio/mp3")
+
+        # Get the transcript
+        audio = BytesIO(audio_content)
+        transcript = openai.Audio.translate("whisper-1", audio, "This transcript is in Hindi.")
+        st.header("Transcript:")
+        st.markdown(transcript["text"])
+        print(transcript)
